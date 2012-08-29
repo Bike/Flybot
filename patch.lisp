c2e88f0ebc38d784164d2992376e2343d0ec040e
James Kalenius <aeshtaer@gmail.com>
2012-08-28 20:31:24 -0700

    Add restart for NO-SUCH-REPLY.
    IRC numerics are not standardized at all, so we should just ignore
    message types we don't know.
    
    main-loop should probably be moved to a nicer place once it's less of a
    patch.

--- patch.lisp
+++ patch.lisp
@@ -13,8 +13,14 @@
 
 (defun main-loop (connection)
   (handler-bind
-      ((error (lambda (e)
-		(format (client-stream connection) "~a ERROR caught by ~a: ~a~%" (format-time) 'main-loop e)
-		(continue e))))
+      ((no-such-reply
+	(lambda (c)
+	  ;; ignore it and keep going
+	  (format (client-stream connection) "Unknown IRC numeric: ~d" (reply-number c))
+	  (invoke-restart 'continue)))
+       (error
+	(lambda (e)
+	  (format (client-stream connection) "~a ERROR caught by ~a: ~a~%" (format-time) 'main-loop e)
+	  (continue e))))
     (loop (with-simple-restart (continue "internal restart")
 		  (unless (read-message connection) (return-from main-loop :done))))))
